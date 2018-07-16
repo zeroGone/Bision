952e1e24bc88be8dce5267e896e6dfc000bd8e79
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2018-07-16 19:53:08 +0900

    0716

--- app/src/main/java/com/example/bision/MainIntro.java
+++ app/src/main/java/com/example/bision/MainIntro.java
@@ -1,14 +1,19 @@
 package com.example.bision;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.*;
+import android.view.View;
 
-public class MainView extends AppCompatActivity {
+
+public class MainIntro extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main_view);
+        setContentView(R.layout.activity_introduce);
+
     }
 }
+
