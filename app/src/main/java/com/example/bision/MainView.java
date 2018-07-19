4a1447996ec17de1053559d1c2686d5b7620ef1b
YoungGon KIM <33260476+zeroGone@users.noreply.github.com>
2018-07-19 21:42:54 +0900

    0719 시발

--- app/src/main/java/com/example/bision/MainView.java
+++ app/src/main/java/com/example/bision/MainView.java
@@ -1,6 +1,7 @@
 package com.example.bision;
 
 import android.content.Intent;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.*;
@@ -10,6 +11,12 @@ public class MainView extends AppCompatActivity {
     HorizontalScrollView deadline_scroll;
     LinearLayout deadlineView;
     LinearLayout deadline1;
+    LinearLayout deadline3;
+    Button deadline1_button;
+    ImageView deadline2_imageView;
+    Button menu_button;
+    DrawerLayout drawer;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,25 +27,40 @@ public class MainView extends AppCompatActivity {
 
         deadlineView = (LinearLayout)findViewById(R.id.deadline_view);
         deadline1 = (LinearLayout)findViewById(R.id.deadline1);
-        deadlineView.setOnTouchListener(new View.OnTouchListener() {
-            @Override
-            public boolean onTouch(View view, MotionEvent motionEvent) {
-                return true;
-            }
-        });
-
-        deadline1.setOnTouchListener(new View.OnTouchListener(){
-            @Override
-            public boolean onTouch(View v, MotionEvent event){
-                return true;
-            }
-        });
-
-        deadline1.setOnClickListener(new View.OnClickListener() {
+        deadline1_button=(Button)findViewById(R.id.deadline1_button);
+        deadline1_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(),MainIntro.class);
+                Intent intent = new Intent(getApplicationContext(),  MainIntro.class);
                 startActivity(intent);
+            }
+        });
+        deadline2_imageView=(ImageView)findViewById(R.id.deadline2_image);
+
+        deadline2_imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),  MainIntro.class);
+                startActivity(intent);
+            }
+        });
+
+        deadline3=(LinearLayout)findViewById(R.id.deadline3);
+        deadline3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(),  MainIntro.class);
+                startActivity(intent);
+            }
+        });
+        drawer = (DrawerLayout) findViewById(R.id.menu_draw);
+        drawer.requestDisallowInterceptTouchEvent(true);
+        menu_button=(Button)findViewById(R.id.menuButton);
+        menu_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(drawer.isDrawerOpen(Gravity.LEFT)) drawer.closeDrawer(Gravity.LEFT);
+                else drawer.openDrawer(Gravity.LEFT);
             }
         });
     }
